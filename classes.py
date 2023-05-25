b9f0e5e0c6ac31665a0ecebd7dabbbf81942d4a8
Fleshka <fleisner.omsk@gmail.com>
2023-05-25 17:10:17 +0600

    Продолжается переписывание)

--- classes.py
+++ classes.py
@@ -1,3 +1,13 @@
+# Импорт библиотек
+import datetime
+import pprint
+
+import requests
+from bs4 import BeautifulSoup
+
+# Импорт данных по сайту
+from dates import *
+
 # Класс общих данных
 class FullDates:
     # Метод-конструктор с переменными экземпляра класса
@@ -6,14 +16,103 @@ class FullDates:
         self.exporturls = []
         # Список запланированных постов с временем
         self.timewithposts = []
+        # Список импортированных новостей
+        self.importurls = []
 
     # Функция печати данных
     def printidates(self):
         print("=Start===Class FullDates==========")
-        print("===========ExportUrls=============\n")
+        print("===========ExportUrls=============")
         for element in self.exporturls:
             print("\t" + element)
-        print("=========TimeWithPosts===========\n")
+        print("=========TimeWithPosts===========")
         for element in self.timewithposts:
             print("\t" + element)
+        print("==========ImportUrls=============")
+        for element in self.importurls:
+            print("\t" + element)
         print("=End=====Class FullDates==========")
+
+    # Функция импорта данных
+    def importurlsfrompage(self):
+
+        # Получаем сегодняшнее число
+        today = datetime.datetime.today()
+        day = today.strftime("%d")
+        intday = int(day)
+        month = today.strftime("%m")
+        months = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"]
+        year = today.strftime("%Y")
+        today = str(intday) + " " + months[int(month) - 1] + " " + year
+        #print(f"\t{today}")
+
+        # Получем данные со страницы
+        try:
+           page = requests.get(urlpage, headers=headers, timeout=5)
+        except requests.exceptions.HTTPError as error:
+            print(error)
+        #print(f"\t{page}")
+
+        # Разбираем страницу с помощью BeautifulSoup
+        html = BeautifulSoup(page.content, 'html.parser')
+        postdates = html.select(".gbnews-listShort > td > a")
+
+        # Попытка решить проблему с вылетом скрипта
+        s = requests.session()
+        s.keep_alive = False
+
+        # Формируем список ссылок на новости
+        pathlist = []
+        i = 0
+        for element in postdates:
+            if i % 2 == 0:
+                pathlist.append(element["href"])
+            i = i + 1
+        dates = html.select(".sub")
+        #pprint.pprint(dates)
+
+        # Формируем массив с ссылками на новости которые надо распарсить
+        result = []
+        i = 0
+        for element in dates:
+            # Ограничение на максимальное количество новостей на странице
+            if i == 20:
+                break
+            # Обрезаем дату поста новости, для сравнения с текущей датой
+            date_post = str(element.text)[:-8]
+            #print(f"\t\t{date_post}")
+            # Если дата поста совпадает с сегодняшней датой, то
+            #if date_post == today:
+            if date_post == "24 мая 2023":
+                # Формируем итоговый массив с ссылками
+                result.append(str(pathlist[i]))
+            i = i + 1
+        # Записываем результат в лист экспортированных url
+        self.exporturls = result
+
+    # Функция планирования постанга новостей
+    def planpostingdates(self):
+        print("123")
+
+# Класс времён
+class times:
+
+    # Время импорта новостей
+    importtime = datetime.datetime.today().strftime("%H:%M")
+    #importtime = datetime.time(23, 55).strftime("%H:%M")
+
+    # Время подготовки плана постинга
+    #planpostingdates = datetime.time(23, 58).strftime("%H:%M")
+    planpostingdates = (datetime.datetime.today() + datetime.timedelta(minutes=1)).strftime("%H:%M")
+    # Время начала постинга
+    starttimeposting = datetime.time(8,00).strftime("%H")
+    # Время конца постинга
+    endtimeposting = datetime.time(23, 00).strftime("%H")
+
+    # Время первого поста
+    #timetopost = datetime.time(8, 00).strftime("%H:%M")
+    timetopost = (datetime.datetime.today() + datetime.timedelta(minutes=3)).strftime("%H:%M")
+
+    # Время обнуления переменных
+    nulltime = datetime.time(23, 0).strftime("%H:%M")
+    #nulltime = (datetime.datetime.today() + datetime.timedelta(minutes=1)).strftime("%H:%M")
--- main.py
+++ main.py
@@ -4,9 +4,60 @@ from classes import *
 # Импорт библиотек
 import time
 
+# Функция определения действия
+def switcher(time, data):
+    match(time):
+        # Время обнуления переменных
+        case times.nulltime:
+            print(f"Время для обнуления переменных: {times.nulltime}")
+            # Обнуляем переменные класса FullDates
+            data.exporturls = []
+            data.timewithposts = []
+            data.importurls = []
+            # Выводим данные в лог
+            dddates.printidates()
+
+        # Время для импортирования новостей с сайта
+        case times.importtime:
+            print("Время для импортирования новостей с сайта: ", times.importtime)
+            # Импорт urls для создания статей с новостями
+            data.importurlsfrompage()
+
+            # Выводим данные в лог
+            dddates.printidates()
+
+
+        # Время для планирования постов
+        case times.planpostingdates:
+            print("Время для планирования постинга новостей с сайта: ", times.planpostingdates)
+            data.printidates()
+            if len(data.exporturls) == 0:
+                print("\tМассив экспортированных новостей пуст")
+            else:
+                print("\tМассив экспортированных новостей не пуст")
+                # Запускаем функцию формирования результирующего списка ссылок для публикации
+                times.timetopost = data.planpostingdates(times.starttimeposting, times.endtimeposting)
+                # Выводим данные в лог
+                dddates.printidates()
+
+        # Время для вывода времени
+        case _:
+            print(f"Время сейчас: {time}")
+    # Возвращаем изменившиеся данные
+    return data
+
+# Создаём экземпляр класса fulldates
+dddates = FullDates()
+
 # Вечный бесконечный цикл
 while True:
-    # Создаём экземпляр класса fulldates
-    dddates = FullDates()
-    dddates.printidates()
-    time.sleep(60)
+
+    try:
+        # Время сейчас
+        today = datetime.datetime.today()
+        todaytime = today.strftime("%H:%M")
+        # Запуск функции выбора действия с возвращением занчений в экземпляр класса
+        dddates = switcher(todaytime, dddates)
+        time.sleep(60)
+    except Exception as e:
+        print(e)
